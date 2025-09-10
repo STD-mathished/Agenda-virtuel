5e817d13e0e1a00ec3a9ff371f976e89b8d3fcae
Mathis Heder <mathis.heder91@gmail.com>
2025-09-10 17:17:38 +0200

    not found page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx';
 import Taches from './pages/taches.tsx';
 import AjouterTache from './pages/ajouterTache.tsx';
+import NotFound from './pages/notFound.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Route path='/' element={<App/>}/>
       <Route path='/taches' element={<Taches/>}/>
       <Route path='/ajouter' element={<AjouterTache/>}/>
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
   </BrowserRouter>,
 )
