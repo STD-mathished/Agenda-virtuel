08bc70101023c092dc797aecbc15c0782456eb6d
Mathis Heder <mathis.heder91@gmail.com>
2025-10-15 15:52:48 +0200

    chore(notFound): traduction en francais de la page et ajout de l'icone dans le titre de la page

--- frontend/src/pages/notFound.tsx
+++ frontend/src/pages/notFound.tsx
@@ -8,13 +8,13 @@ export default function NotFound() {
             <h1
                 className="text-2xl font-bold"
             > 
-                The page you are looking for has not been found !
+                La page que vous cherchez n'existe pas
             </h1>
             <Link 
                 to={"/"}
                 className="text-xl underline text-blue-400"
             >
-                Go back to the app
+                Retourner à la page d'accueil
             </Link>
         </section>
     )
