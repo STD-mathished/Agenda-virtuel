2053aabccc619007abf2460801feb0a084419135
Mathis Heder <mathis.heder91@gmail.com>
2026-02-19 21:25:56 +0100

    feat:dockerisation du backend + bd

--- backend/scripts/constants.py
+++ backend/scripts/constants.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 
 # Definission de la base de données
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
-DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER", "user_agenda")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password_agenda")
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "agenda_db")
